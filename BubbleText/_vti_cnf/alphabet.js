a1200de8a5e06208e4a4ca29ca73364dfc3d5947
Adam Price <adam.price56@gmail.com>
2014-03-06 23:31:22 +0000

    Added Colour Changing
    Added the ability to change the colours of letters in any order (Up to 5
    different colours).

---
+++ BubbleText/_vti_cnf/alphabet.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|23 Feb 2014 00:09:48 -0000
+vti_author:SR|ADAM-LAPTOP\\Adam
+vti_modifiedby:SR|ADAM-LAPTOP\\Adam
+vti_nexttolasttimemodified:TW|22 Feb 2014 18:19:09 -0000
+vti_timecreated:TR|22 Feb 2014 18:20:09 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|BubbleText/default.html BubbleText/backup.html
+vti_cacheddtm:TX|23 Feb 2014 00:09:48 -0000
+vti_filesize:IR|17679
---
+++ BubbleText/_vti_cnf/bubbles.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|06 Mar 2014 22:54:57 -0000
+vti_author:SR|ADAM-LAPTOP\\Adam
+vti_modifiedby:SR|ADAM-LAPTOP\\Adam
+vti_nexttolasttimemodified:TW|06 Mar 2014 22:48:07 -0000
+vti_timecreated:TR|22 Feb 2014 18:20:09 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|BubbleText/default.html BubbleText/backup.html
+vti_cacheddtm:TX|23 Feb 2014 00:09:45 -0000
+vti_filesize:IR|10504
